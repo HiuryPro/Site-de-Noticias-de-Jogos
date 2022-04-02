ff711a4fd446493491732a7c1dd11032ff4db4b8
HiuryPro <hiurylucas@unipam.edu.br>
2022-04-02 12:06:55 -0300

    Site de Noticias de jogos
    HTML ,CSS , Javascript e Bootstrap do site.

---
+++ projeto_integrador Black theme/enviar.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = addslashes($_POST['nome']); 
+$phone = addslashes($_POST['phone']); 
+$mensagem = addslashes($_POST['mensagem']);
+$email = addslashes($_POST['email']);
+
+
+$to = "ickydangerbr13@gmail.com";
+
+?>
+
+
---
+++ projeto_integrador Black theme/js/script.js
@@ -0,0 +1,8 @@
+'npm i --save-dev @types/jquery'
+function myFunction() {
+ 
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+    
+}
